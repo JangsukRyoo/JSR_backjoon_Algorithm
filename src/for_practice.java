09e90ef743d4814856984a44a6df329e41c77f44
jsr <jangsukr@gmail.com>
2021-11-21 00:52:51 +0900

    단계 풀이 while

--- src/for_practice.java
+++ src/for_practice.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class for_practice {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
 
-
+    }
 }
---
+++ src/while_practice.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class while_practice {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        //  A + B -5 10952
+
+//        do {
+//            int a = sc.nextInt();
+//            int b = sc.nextInt();
+//            if ((a + b) == 0) {
+//                break;
+//            }
+//            System.out.println(a + b);
+//        } while (true);
+
+        //  A + B +4
+//        while(sc.hasNext()){
+//            int a = sc.nextInt();
+//            int b = sc.nextInt();
+//            System.out.println(a + b);
+//        }
+
+
+        // 더하기 사이클 #1110
+
+        String num = sc.nextLine();
+        int a,b;
+        int ct = 0;
+        int ini = 0;
+        int sum = 0;
+        if(num.length() == 1){
+            a = 0;
+            b = (int)num.charAt(0)-'0';
+        }else{
+          a = (int)num.charAt(0)-'0';
+          b = (int)num.charAt(1)-'0';
+        }
+        ini = (a*10)+b;
+        while(true){
+            sum = a+b;
+            a=b;
+            b=(sum%10);
+            int newNum = (a*10)+b;
+            ct ++;
+            if(ini == newNum){
+               break;
+            }
+        }
+        System.out.println(ct);
+    }
+}
