574875c3266516d7da10c5ceb82de894a7422659
jsr <jangsukr@gmail.com>
2021-11-29 00:39:38 +0900

    문자열 #5 추가

--- src/String_practice.java
+++ src/String_practice.java
@@ -53,6 +53,31 @@ public class String_practice {
 //            System.out.println();
 //        }
 
+        // 단어공부 #1157
+        int[] arr = new int[26];
+        String s = sc.next();
 
+        for (int i = 0; i < s.length(); i++){
+
+            if ('A' <= s.charAt(i) && s.charAt(i) <= 'Z') {
+                arr[s.charAt(i) - 'A']++;
+            }
+
+            else {
+                arr[s.charAt(i) - 'a']++;
+            }
+        }
+        int max = -1;
+        char ch = '?';
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                ch = (char) (i + 65);
+            }
+            else if (arr[i] == max) {
+                ch = '?';
+            }
+        }
+        System.out.print(ch);
     }
 }
