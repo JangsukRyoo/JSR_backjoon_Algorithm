96c1247539f0dee2528be94534e069bb482a5381
jsr <jangsukr@gmail.com>
2021-11-28 23:33:31 +0900

    문자열 수정

--- src/String_practice.java
+++ src/String_practice.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class String_practice {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.next().charAt(0);
         System.out.println(a);
