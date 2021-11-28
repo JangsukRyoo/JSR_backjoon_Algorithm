4db4338e50669d3ac0614c1d4bb7073ac4a67fe6
jsr <jangsukr@gmail.com>
2021-11-29 00:07:15 +0900

    문자열 #3 추가

--- src/String_practice.java
+++ src/String_practice.java
@@ -3,7 +3,41 @@ import java.util.Scanner;
 public class String_practice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.next().charAt(0);
-        System.out.println(a);
+
+        //아스키 코드  #11654
+//        int a = sc.next().charAt(0);
+//        System.out.println(a);
+
+        //숫자의 합 #11720
+//        int n = sc.nextInt();
+//        String a = sc.next();
+//        int sum = 0;
+//
+//        for(int i =0; i< n; i++){
+//            sum += a.charAt(i)-'0';
+//        }
+//        System.out.println(sum);
+
+        //알파벳 찾기 #10809
+//        int[] arr = new int[26];
+//        String a = sc.nextLine();
+//
+//        for(int i=0; i< arr.length; i++){
+//            arr[i] = -1;
+//        }
+//
+//        for(int j=0; j < a.length(); j++){
+//            char cha = a.charAt(j);
+//
+//            if(arr[cha-'a'] == -1){
+//                arr[cha-'a'] = j;
+//            }
+//        }
+//        for(int val : arr){
+//            System.out.println(val + " ");
+//        }
+
+
+
     }
 }
