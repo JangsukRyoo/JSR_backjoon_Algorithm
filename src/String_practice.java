0754c956b34d27d8151dbed6436cc59d87552ef7
jsr <jangsukr@gmail.com>
2021-11-30 23:27:11 +0900

    문자열 #6 추가

--- src/String_practice.java
+++ src/String_practice.java
@@ -54,30 +54,63 @@ public class String_practice {
 //        }
 
         // 단어공부 #1157
-        int[] arr = new int[26];
-        String s = sc.next();
+//        int[] arr = new int[26];
+//        String s = sc.next();
+//
+//        for (int i = 0; i < s.length(); i++){
+//
+//            if ('A' <= s.charAt(i) && s.charAt(i) <= 'Z') {
+//                arr[s.charAt(i) - 'A']++;
+//            }
+//
+//            else {
+//                arr[s.charAt(i) - 'a']++;
+//            }
+//        }
+//        int max = -1;
+//        char ch = '?';
+//        for (int i = 0; i < 26; i++) {
+//            if (arr[i] > max) {
+//                max = arr[i];
+//                ch = (char) (i + 65);
+//            }
+//            else if (arr[i] == max) {
+//                ch = '?';
+//            }
+//        }
+//        System.out.print(ch);
+//    }
 
-        for (int i = 0; i < s.length(); i++){
+        //단어의 개수 # 1152
 
-            if ('A' <= s.charAt(i) && s.charAt(i) <= 'Z') {
-                arr[s.charAt(i) - 'A']++;
-            }
+        String str = sc.nextLine().trim();
 
-            else {
-                arr[s.charAt(i) - 'a']++;
-            }
+        if(str.isEmpty()){
+            System.out.println("0");
+        } else{
+            System.out.println(str.split(" ").length);
         }
-        int max = -1;
-        char ch = '?';
-        for (int i = 0; i < 26; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-                ch = (char) (i + 65);
-            }
-            else if (arr[i] == max) {
-                ch = '?';
-            }
-        }
-        System.out.print(ch);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
