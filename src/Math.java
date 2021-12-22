153a07e2fcfaff8e9cd7ff59e6d9046989d3d02a
jsr <jangsukr@gmail.com>
2021-12-22 23:13:02 +0900

    기본 수학 1-3

--- src/Math.java
+++ src/Math.java
@@ -17,19 +17,42 @@ public class Math {
 //        }
 
         //  #2292 벌집
+//        int n = sc.nextInt();
+//        int ct = 1;
+//        int range = 2;
+//
+//        if (n ==1) {
+//            System.out.println(1);
+//        }else{
+//            while(range <= n){
+//                range = range + (6 *ct);
+//                ct ++;
+//            }
+//            System.out.println(ct);
+//        }
 
-        int n = sc.nextInt();
-        int ct = 1;
-        int range = 2;
+        // #1193
+        int X = sc.nextInt();
 
-        if (n ==1) {
-            System.out.println(1);
-        }else{
-            while(range <= n){
-                range = range + (6 *ct);
-                ct ++;
+        int crossCt = 1;
+        int prev_sum = 0;
+
+        while (true) {
+
+            if (X <= prev_sum + crossCt) {
+
+                if (crossCt % 2 == 1) {
+                    System.out.print((crossCt - (X - prev_sum - 1)) + "/" + (X - prev_sum));
+                    break;
+                }
+                else {
+                    System.out.print((X - prev_sum) + "/" + (crossCt - (X - prev_sum - 1)));
+                    break;
+                }
+            } else {
+                prev_sum += crossCt;
+                crossCt++;
             }
-            System.out.println(ct);
         }
     }
 }
