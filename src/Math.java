fb677404e4e8bf4a08637f3b785cca582701ed1d
jsr <jangsukr@gmail.com>
2021-12-22 23:05:17 +0900

    기본 수학 1-2

--- src/Math.java
+++ src/Math.java
@@ -1,20 +1,35 @@
 import java.util.Scanner;
 
 public class Math {
-    public static void main(String[] args){
+    public static void main(String args[]){
 
         Scanner sc = new Scanner(System.in);
 
         // #1712 손익 분기점
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c = sc.nextInt();
+//
+//        if(c <= b){
+//            System.out.println("-1");
+//        }else{
+//            System.out.println((a/(c-b))+1);
+//        }
 
-        if(c <= b){
-            System.out.println("-1");
+        //  #2292 벌집
+
+        int n = sc.nextInt();
+        int ct = 1;
+        int range = 2;
+
+        if (n ==1) {
+            System.out.println(1);
         }else{
-            System.out.println((a/(c-b))+1);
+            while(range <= n){
+                range = range + (6 *ct);
+                ct ++;
+            }
+            System.out.println(ct);
         }
     }
-
 }
