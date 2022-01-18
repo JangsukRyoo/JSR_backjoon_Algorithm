5a271892405d6746ce99ba15634251fdf52e18ff
jsr <jangsukr@gmail.com>
2022-01-18 23:16:51 +0900

    기본 수학 1-8

--- src/Math.java
+++ src/Math.java
@@ -92,29 +92,29 @@ public class Math {
 //        }
 
 //      #2775
-
-        int testCase = sc.nextInt();
-        int[][] apt = new int[15][15];
-
-        apt[0][0] =1;
-
-        for( int i =0; i<apt.length; i++){
-            apt[0][i] = i+1;
-            apt[i][0] = 1;
-        }
-
-        for(int i =1 ; i<apt.length; i++){
-            for(int j =1; j < apt.length; j++){
-                apt[i][j] = apt[i-1][j] + apt[i][j-1];
-            }
-        }
-
-        while(testCase -- > 0){
-            int floor = sc.nextInt();
-            int roomNum = sc.nextInt();
-            System.out.println(apt[floor][roomNum-1]);
-
-        }
+//
+//        int testCase = sc.nextInt();
+//        int[][] apt = new int[15][15];
+//
+//        apt[0][0] =1;
+//
+//        for( int i =0; i<apt.length; i++){
+//            apt[0][i] = i+1;
+//            apt[i][0] = 1;
+//        }
+//
+//        for(int i =1 ; i<apt.length; i++){
+//            for(int j =1; j < apt.length; j++){
+//                apt[i][j] = apt[i-1][j] + apt[i][j-1];
+//            }
+//        }
+//
+//        while(testCase -- > 0){
+//            int floor = sc.nextInt();
+//            int roomNum = sc.nextInt();
+//            System.out.println(apt[floor][roomNum-1]);
+//
+//        }
 
 
    }
