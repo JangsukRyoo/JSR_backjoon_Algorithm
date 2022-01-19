f5f25b9d8320d258d532e2c472f98ce65ee7b093
jsr <jangsukr@gmail.com>
2022-01-19 23:57:15 +0900

    기본 수학 1-8 이름변경

--- src/MathProblem.java
+++ src/MathProblem.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Math {
+public class MathProblem {
     public static void main(String args[]) throws IOException {
 
         Scanner sc = new Scanner(System.in);
