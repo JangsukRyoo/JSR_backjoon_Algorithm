ec2af135164517251a9fce8c1376638914ae04e0
jsr <jangsukr@gmail.com>
2022-01-22 23:40:18 +0900

    기본 수학 2-1

---
+++ src/MathProblem2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class MathProblem2 {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+//        #1978
+
+        int n = sc.nextInt();
+        int ct = 0;
+
+        for(int i =0; i<n; i++){
+
+            boolean isPrime = true;
+            int num = sc.nextInt();
+
+            if(num == 1){
+                continue;
+            }
+
+            for(int j = 2; j <=Math.sqrt(num); j++){
+                if(num % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                ct ++;
+            }
+        }
+        System.out.println(ct);
+
+    }
+}
