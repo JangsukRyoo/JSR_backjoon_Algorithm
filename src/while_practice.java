bf5eb94ea8b116aa4448503c67f4d8a70a5c7956
jsr <jangsukr@gmail.com>
2022-01-25 23:44:59 +0900

    #11653 인수분해

--- src/while_practice.java
+++ src/while_practice.java
@@ -25,30 +25,44 @@ public class while_practice {
 
 
         // 더하기 사이클 #1110
+//        String num = sc.nextLine();
+//        int a,b;
+//        int ct = 0;
+//        int ini = 0;
+//        int sum = 0;
+//        if(num.length() == 1){
+//            a = 0;
+//            b = (int)num.charAt(0)-'0';
+//        }else{
+//          a = (int)num.charAt(0)-'0';
+//          b = (int)num.charAt(1)-'0';
+//        }
+//        ini = (a*10)+b;
+//        while(true){
+//            sum = a+b;
+//            a=b;
+//            b=(sum%10);
+//            int newNum = (a*10)+b;
+//            ct ++;
+//            if(ini == newNum){
+//               break;
+//            }
+//        }
+//        System.out.println(ct);
 
-        String num = sc.nextLine();
-        int a,b;
-        int ct = 0;
-        int ini = 0;
-        int sum = 0;
-        if(num.length() == 1){
-            a = 0;
-            b = (int)num.charAt(0)-'0';
-        }else{
-          a = (int)num.charAt(0)-'0';
-          b = (int)num.charAt(1)-'0';
-        }
-        ini = (a*10)+b;
-        while(true){
-            sum = a+b;
-            a=b;
-            b=(sum%10);
-            int newNum = (a*10)+b;
-            ct ++;
-            if(ini == newNum){
-               break;
+        //11653
+
+        int n = sc.nextInt();
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            while (n % i == 0) {
+                System.out.println(i);
+                n /= i;
             }
         }
-        System.out.println(ct);
+        if (n != 1) {
+            System.out.println(n);
+        }
     }
 }
+
