e8a4ef06e24796283a645ec9d0241a52e35db703
jsr <jangsukr@gmail.com>
2022-01-26 22:04:00 +0900

    #11653 소수 구하기

--- src/while_practice.java
+++ src/while_practice.java
@@ -52,17 +52,17 @@ public class while_practice {
 
         //11653
 
-        int n = sc.nextInt();
-
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            while (n % i == 0) {
-                System.out.println(i);
-                n /= i;
-            }
-        }
-        if (n != 1) {
-            System.out.println(n);
-        }
+//        int n = sc.nextInt();
+//
+//        for (int i = 2; i <= Math.sqrt(n); i++) {
+//            while (n % i == 0) {
+//                System.out.println(i);
+//                n /= i;
+//            }
+//        }
+//        if (n != 1) {
+//            System.out.println(n);
+//        }
     }
 }
 
