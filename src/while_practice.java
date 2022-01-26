503a16f563ef06c36b2939d16ddf9d6b1690dcc9
jsr <jangsukr@gmail.com>
2022-01-26 22:08:51 +0900

    #11653 베트랑공준

--- src/while_practice.java
+++ src/while_practice.java
@@ -67,26 +67,28 @@ public class while_practice {
         //1929
         //소수 구하기
 
-        int m = sc.nextInt();
-        int n = sc.nextInt();
-
-        boolean[] prime = new boolean[n + 1];
-        get_prime();
-
-        for(int i = m; i <= n; i++) {
-            // false = 소수
-            if(!prime[i]) System.out.println(i);
-        }
-    }
-
-    public static void get_prime() {
-        prime[0] = prime[1] = true;
-        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
-            if(prime[i]) continue;
-            for(int j = i * i; j < prime.length; j += i) {
-                prime[j] = true;
-            }
-        }
+//        int m = sc.nextInt();
+//        int n = sc.nextInt();
+//
+//        boolean[] prime = new boolean[n + 1];
+//        get_prime();
+//
+//        for(int i = m; i <= n; i++) {
+//            // false = 소수
+//            if(!prime[i]) System.out.println(i);
+//        }
+//    }
+//
+//    public static void get_prime() {
+//        prime[0] = prime[1] = true;
+//        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+//            if(prime[i]) continue;
+//            for(int j = i * i; j < prime.length; j += i) {
+//                prime[j] = true;
+//            }
+//        }
+        
+        //4948 베트랑 공준
     }
 }
 
