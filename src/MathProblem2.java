3a0b975bcbe28b12028f713e7233b788902f8e60
JangsukRyoo <jangsukr@gmail.com>
2022-01-29 02:06:16 +0900

    macbook commit Test

--- src/MathProblem2.java
+++ src/MathProblem2.java
@@ -33,42 +33,42 @@ public class MathProblem2 {
 //        System.out.println(ct);
 
 //        #2581
-
-        int M = sc.nextInt();
-        int N = sc.nextInt();
-
-        prime = new boolean[N + 1];
-        get_prime();
-
-        int sum = 0;
-        int min = Integer.MAX_VALUE;
-        for(int i = M; i <= N; i++) {
-            if(prime[i] == false) {
-                sum += i;
-                if(min == Integer.MAX_VALUE) {
-                    min = i;
-                }
-            }
-        }
-
-        if(sum == 0) {
-            System.out.println(-1);
-        }
-        else {
-            System.out.println(sum);
-            System.out.println(min);
-        }
-    }
-
-    public static void get_prime() {
-        prime[0] = true;
-        prime[1] = true;
-
-        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
-            for(int j = i * i; j < prime.length; j += i) {
-                prime[j] = true;
-            }
-        }
+//
+//        int M = sc.nextInt();
+//        int N = sc.nextInt();
+//
+//        prime = new boolean[N + 1];
+//        get_prime();
+//
+//        int sum = 0;
+//        int min = Integer.MAX_VALUE;
+//        for(int i = M; i <= N; i++) {
+//            if(prime[i] == false) {
+//                sum += i;
+//                if(min == Integer.MAX_VALUE) {
+//                    min = i;
+//                }
+//            }
+//        }
+//
+//        if(sum == 0) {
+//            System.out.println(-1);
+//        }
+//        else {
+//            System.out.println(sum);
+//            System.out.println(min);
+//        }
+//    }
+//
+//    public static void get_prime() {
+//        prime[0] = true;
+//        prime[1] = true;
+//
+//        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+//            for(int j = i * i; j < prime.length; j += i) {
+//                prime[j] = true;
+//            }
+//        }
 
     }
 }
