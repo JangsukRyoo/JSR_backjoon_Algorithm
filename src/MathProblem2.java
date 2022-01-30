91a9741e97a563b14cf24fa16d6de3060ae332ae
JangsukRyoo <jangsukr@gmail.com>
2022-01-30 23:54:57 +0900

    math 2 #9020 수정

--- src/MathProblem2.java
+++ src/MathProblem2.java
@@ -77,18 +77,13 @@ public class MathProblem2 {
         // 2보다  큰 짝수가 주어질 때 소수의 합을 구한다.
         // 소수의 합이 여러개일 경우 두 소수의 차이가 작은 것을 출력한다
         // 두 개으 소수를 출력 할 때 작은 소수 부터 출력한다.
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         get_prime();
-
         int T = Integer.parseInt(br.readLine());
-
         while (T-- > 0){
             int n = Integer.parseInt(br.readLine());
             int first = n/2;
             int second = n/2;
-
             if (!prime[first] && !prime[second]) {
                 System.out.println(first + " " + second);
                 break;
@@ -97,7 +92,6 @@ public class MathProblem2 {
             second++;
         }
     }
-
     public static void get_prime() {
         prime[0] = prime[1] = true;
 
