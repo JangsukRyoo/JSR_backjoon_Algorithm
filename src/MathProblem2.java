4373447fca2d3e2da285aa567c0bdab9af63f407
Jangsuk Ryoo <jangsukr@gmail.com>
2022-02-05 23:54:38 +0900

    math 2 #1085 정리

--- src/MathProblem2.java
+++ src/MathProblem2.java
@@ -102,7 +102,26 @@ public class MathProblem2 {
 //
 //        System.out.println(Math.min(x_min, y_min);
 
+//      #3009
 
+        int[][] arr = new int[3][2];;
+        int x=0, y=0;
+
+        for(int i=0;i<3;i++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+
+        if(arr[0][0] == arr[1][0]) x = arr[2][0];
+        else if(arr[0][0] == arr[2][0]) x = arr[1][0];
+        else if(arr[1][0] == arr[2][0]) x = arr[0][0];
+
+        if(arr[0][1] == arr[1][1]) y = arr[2][1];
+        else if(arr[0][1] == arr[2][1]) y = arr[1][1];
+        else if(arr[1][1] == arr[2][1]) y = arr[0][1];
+
+        System.out.println(x + " " + y);
+    }
 
        }
     }
@@ -119,7 +138,6 @@ public class MathProblem2 {
 //        }
 //    }
 //
-//    public static boolean[] prim = new boolean[10001];
-}
-        }
-        }
+//  public static boolean[] prim = new boolean[10001];
+
+
