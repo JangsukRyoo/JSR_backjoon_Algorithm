25237ff6e049c6e658b4f9dc891bf5e82fae812f
Jangsuk Ryoo <jangsukr@gmail.com>
2022-02-06 23:50:36 +0900

    math 2 #4153 코드정리

--- src/MathProblem2.java
+++ src/MathProblem2.java
@@ -126,25 +126,25 @@ public class MathProblem2 {
 //
 //        #4153
 
-        while (true) {
-
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            int z = sc.nextInt();
-
-            if (x == 0 && y == 0 && z == 0) break;
-
-            if ((x * x + y * y) == z * z) {
-                System.out.println("right");
-            } else if (x * x == (y * y + z * z)) {
-                System.out.println("right");
-            } else if (y * y == (z * z + x * x)) {
-                System.out.println("right");
-            } else {
-                System.out.println("wrong");
-            }
-
-        }
+//        while (true) {
+//
+//            int x = sc.nextInt();
+//            int y = sc.nextInt();
+//            int z = sc.nextInt();
+//
+//            if (x == 0 && y == 0 && z == 0) break;
+//
+//            if ((x * x + y * y) == z * z) {
+//                System.out.println("right");
+//            } else if (x * x == (y * y + z * z)) {
+//                System.out.println("right");
+//            } else if (y * y == (z * z + x * x)) {리
+//                System.out.println("right");
+//            } else {
+//                System.out.println("wrong");
+//            }
+//
+//        }
     }
 }
 
@@ -162,5 +162,5 @@ public class MathProblem2 {
 //    }
 //
 //  public static boolean[] prim = new boolean[10001];
-
+₩
 
