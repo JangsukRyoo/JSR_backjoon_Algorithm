f11439e8362e1cedfb58bee5205ea842a1b69383
Jangsuk Ryoo <jangsukr@gmail.com>
2022-02-08 22:02:36 +0900

    재귀 - 피보나치 5

--- src/Recursive.java
+++ src/Recursive.java
@@ -16,7 +16,7 @@ public class Recursive {
         int[] fibonacci = new int[n + 1];
 
         for(int i = 0; i < fibonacci.length; i++) {
-            // F(0) 과 F(1) 은 각각 0 과 1 로 초기화
+
             if(i == 0) fibonacci[0] = 0;
             else if(i == 1) fibonacci[1] = 1;
             else fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
