f3f8e5427e916fb76c31d31ff5d0222b37b36d5c
Jangsuk Ryoo <jangsukr@gmail.com>
2022-07-27 23:19:36 +0900

    Update GuGuDan.java

--- src/For_Problem/GuGuDan.java
+++ src/For_Problem/GuGuDan.java
@@ -1,11 +1,10 @@
 package For_Problem;
 
 import java.util.Scanner;
-
+        //구구단 #2793
 public class GuGuDan {
     public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
-        //구구단 #2793
         int a = sc.nextInt();
         for(int i =1; i< 10; i++){
             System.out.println(a + " * " + i + " = " + (a*i));
