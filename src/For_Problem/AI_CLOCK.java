17044f5e4e277e2bd47601a90a3ad46db2bd154b
Jangsuk Ryoo <jangsukr@gmail.com>
2023-01-22 19:32:35 +0900

    사칙연산 문제 추가 태국에서의 생일, 인공지능 시계

---
+++ src/For_Problem/AI_CLOCK.java
@@ -0,0 +1,33 @@
+package For_Problem;
+
+import java.util.*;
+
+public class AI_CLOCK {
+    public static void main(String args[]) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        String ent = sc.nextLine();
+        int entSec = sc.nextInt();
+
+        String[] spl = ent.split("\\s");
+
+        int hr = Integer.parseInt(spl[0]);
+        int min = Integer.parseInt(spl[1]);
+        int sec = Integer.parseInt(spl[2]);
+
+        // 60초가 넘어가는 몫은 분에 더하고 나머지는 초에 넣고
+        sec += entSec;
+        min += (sec/60);
+
+        // 60초가 넘어가는 몫은 시에 더하고 나머지는 분에 넣고
+        sec %= 60;
+        hr +=(min/60);
+        min %= 60;
+
+        hr %=24;
+
+        System.out.println(hr + " " + min + " " + sec);
+    }
+}
+
+
---
+++ src/For_Problem/Taiwan_BirthYear.java
@@ -0,0 +1,14 @@
+package For_Problem;
+
+import java.util.Scanner;
+
+public class Taiwan_BirthYear {
+    public static void main(String args[]) throws Exception {
+        // 1998년생인 내가 태국에서는 2541년생?!?
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a-543);
+    }
+}
+
+
